bab2ffde05cd6f13d4e9efeb62e1122bed19bd7b
AnnulusGames <annulusgames@gmail.com>
2023-09-24 18:29:56 +0900

    Change: allow RegisterEventAttribute multiple

--- Assets/EntitiesEvents/Runtime/RegisterEventAttribute.cs
+++ Assets/EntitiesEvents/Runtime/RegisterEventAttribute.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace EntitiesEvents
 {
-    [AttributeUsage(AttributeTargets.Assembly)]
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     public sealed class RegisterEventAttribute : Attribute
     {
         public RegisterEventAttribute(Type type) { }
