a2eb3ea99f5b1e3d349f00a410f5d203131f27c7
Juha <kukiyomenaisapp@gmail.com>
2023-11-26 23:49:19 +0900

    EventHelperにEntityManagerの拡張を追加

--- Assets/EntitiesEvents/Runtime/EventHelper.cs
+++ Assets/EntitiesEvents/Runtime/EventHelper.cs
@@ -1,6 +1,6 @@
+using EntitiesEvents.Internal;
 using Unity.Collections;
 using Unity.Entities;
-using EntitiesEvents.Internal;
 
 namespace EntitiesEvents
 {
@@ -9,7 +9,7 @@ namespace EntitiesEvents
         public static EventWriter<T> GetEventWriter<T>(this ref SystemState state)
             where T : unmanaged
         {
-            return GetOrCreateSingleton<T>(ref state).events.GetWriter();
+            return GetOrCreateSingleton<T>(state.EntityManager).events.GetWriter();
         }
 
         public static EventWriter<T> GetEventWriter<T>(this SystemBase systemBase)
@@ -18,10 +18,16 @@ namespace EntitiesEvents
             return GetEventWriter<T>(ref systemBase.CheckedStateRef);
         }
 
+        public static EventWriter<T> GetEventWriter<T>(this EntityManager entityManager)
+            where T : unmanaged
+        {
+            return GetOrCreateSingleton<T>(entityManager).events.GetWriter();
+        }
+
         public static EventReader<T> GetEventReader<T>(this ref SystemState state)
             where T : unmanaged
         {
-            return GetOrCreateSingleton<T>(ref state).events.GetReader();
+            return GetOrCreateSingleton<T>(state.EntityManager).events.GetReader();
         }
 
         public static EventReader<T> GetEventReader<T>(this SystemBase systemBase)
@@ -30,25 +36,35 @@ namespace EntitiesEvents
             return GetEventReader<T>(ref systemBase.CheckedStateRef);
         }
 
-        public unsafe static void EnsureBufferCapacity<T>(ref SystemState state, int capacity)
+        public static EventReader<T> GetEventReader<T>(this EntityManager entityManager)
             where T : unmanaged
         {
-            var events = GetOrCreateSingleton<T>(ref state).events;
+            return GetOrCreateSingleton<T>(entityManager).events.GetReader();
+        }
+
+        public static unsafe void EnsureBufferCapacity<T>(ref SystemState state, int capacity)
+            where T : unmanaged
+        {
+            var events = GetOrCreateSingleton<T>(state.EntityManager).events;
             events.GetBuffer()->EnsureCapacity(capacity);
         }
 
-        static EventSingleton<T> GetOrCreateSingleton<T>(ref SystemState state)
+        public static unsafe void EnsureBufferCapacity<T>(EntityManager entityManager, int capacity)
             where T : unmanaged
         {
-            var query = state.GetEntityQuery(ComponentType.ReadWrite<EventSingleton<T>>());
+            var events = GetOrCreateSingleton<T>(entityManager).events;
+            events.GetBuffer()->EnsureCapacity(capacity);
+        }
+
+        private static EventSingleton<T> GetOrCreateSingleton<T>(EntityManager entityManager)
+            where T : unmanaged
+        {
+            var query = entityManager.CreateEntityQuery(ComponentType.ReadWrite<EventSingleton<T>>());
             if (query.TryGetSingleton<EventSingleton<T>>(out var singleton)) return singleton;
-            else
-            {
-                var events = new Events<T>(512, Allocator.Persistent);
-                singleton = new EventSingleton<T> { events = events };
-                state.EntityManager.CreateSingleton(singleton);
-                return singleton;
-            }
+            var events = new Events<T>(512, Allocator.Persistent);
+            singleton = new EventSingleton<T> { events = events };
+            entityManager.CreateSingleton(singleton);
+            return singleton;
         }
     }
 }
